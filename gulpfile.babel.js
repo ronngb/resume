c7fadf832c26c991f25487ad97272a4b7f16bf56
ronngb <ronny.gabi@gmail.com>
2023-01-08 17:23:44 +0800

    chore: append new build tools

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,9 @@ import gulpSass from "gulp-sass";
 import gulpif from "gulp-if";
 import changed from "gulp-changed";
 import sourcemaps from "gulp-sourcemaps";
+import autoprefixer from "gulp-autoprefixer";
+import webpack from "webpack-stream";
+import rename from "gulp-rename";
 import bs from "browser-sync";
 import del from "del";
 
@@ -18,6 +21,10 @@ const paths = {
   styles: {
     src: "src/assets/scss/main.scss",
     dest: "dist/assets/css",
+  },
+  scripts: {
+    src: "src/assets/js/main.js",
+    dest: "dist/assets/js",
   },
   icons: {
     src: "node_modules/@fortawesome/fontawesome-free/webfonts/*",
@@ -37,9 +44,49 @@ export const styles = () => {
         .sync(gulpif(PROD, { outputStyle: "compressed" }))
         .on("error", sass.logError)
     )
+    .pipe(
+      gulpif(
+        PROD,
+        rename({
+          extname: ".min.css",
+        })
+      )
+    )
+    .pipe(autoprefixer())
     .pipe(gulpif(!PROD, sourcemaps.write("./maps")))
     .pipe(gulp.dest(paths.styles.dest))
     .pipe(bs.stream());
+};
+
+export const scripts = () => {
+  return gulp
+    .src(paths.scripts.src)
+    .pipe(
+      webpack({
+        module: {
+          rules: [
+            {
+              test: /\.js$/,
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: [
+                    ["@babel/preset-env", { targets: "> 0.25%, not dead" }],
+                  ],
+                },
+              },
+            },
+          ],
+        },
+        output: {
+          filename: PROD ? "main.min.js" : "main.js",
+        },
+        devtool: !PROD ? "inline-source-map" : false,
+        mode: PROD ? "production" : "development",
+      })
+    )
+    .pipe(bs.stream())
+    .pipe(gulp.dest(paths.scripts.dest));
 };
 
 export const icons = () => {
@@ -64,10 +111,11 @@ export const reload = (done) => {
 
 export const watch = () => {
   gulp.watch("src/assets/scss/**/*.scss", styles);
+  gulp.watch("src/assets/js/**/*.js", scripts);
   gulp.watch("**/*.html", reload);
 };
 
-export const dev = gulp.series(clean, styles, icons, serve, watch);
-export const prod = gulp.series(clean, styles, icons);
+export const dev = gulp.series(clean, styles, scripts, icons, serve, watch);
+export const prod = gulp.series(clean, styles, scripts, icons);
 
 exports.default = dev;
