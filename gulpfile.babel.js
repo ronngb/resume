7d0a3ba475ce979a5eeae91a6c5ee3da79d141db
ronngb <ronny.gabi@gmail.com>
2023-01-28 01:36:25 +0800

    refactor: reconstruct the file hierarchy for deployment in gh-pages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,8 +6,9 @@ import gulpif from "gulp-if";
 import changed from "gulp-changed";
 import sourcemaps from "gulp-sourcemaps";
 import autoprefixer from "gulp-autoprefixer";
-import webpack from "webpack-stream";
+import imagemin from "gulp-imagemin";
 import rename from "gulp-rename";
+import webpack from "webpack-stream";
 import bs from "browser-sync";
 import del from "del";
 
@@ -19,16 +20,27 @@ const PROD = yargs.argv.prod ?? false;
 
 const paths = {
   styles: {
-    src: "src/assets/scss/main.scss",
-    dest: "dist/assets/css",
+    src: "assets/scss/main.scss",
+    dest: {
+      dev: "assets/css",
+      prod: "dist/assets/css",
+    },
   },
   scripts: {
-    src: "src/assets/js/main.js",
+    src: "assets/js/main.js",
     dest: "dist/assets/js",
   },
+  images: {
+    src: "assets/images/**/*.{jpg,jpeg,png}",
+    dest: "dist/assets/images",
+  },
+
   icons: {
     src: "node_modules/@fortawesome/fontawesome-free/webfonts/*",
-    dest: "dist/assets/webfonts",
+    dest: {
+      dev: "assets/webfonts",
+      prod: "dist/assets/webfonts",
+    },
   },
 };
 
@@ -37,7 +49,9 @@ export const clean = () => del(["dist/*/"]);
 export const styles = () => {
   return gulp
     .src(paths.styles.src)
-    .pipe(changed(paths.styles.dest, { hasChanged: changed.compareContents }))
+    .pipe(
+      changed(paths.styles.dest.dev, { hasChanged: changed.compareContents })
+    )
     .pipe(gulpif(!PROD, sourcemaps.init()))
     .pipe(
       sass
@@ -54,7 +68,13 @@ export const styles = () => {
     )
     .pipe(autoprefixer())
     .pipe(gulpif(!PROD, sourcemaps.write("./maps")))
-    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(
+      gulpif(
+        PROD,
+        gulp.dest(paths.styles.dest.prod),
+        gulp.dest(paths.styles.dest.dev)
+      )
+    )
     .pipe(bs.stream());
 };
 
@@ -62,28 +82,31 @@ export const scripts = () => {
   return gulp
     .src(paths.scripts.src)
     .pipe(
-      webpack({
-        module: {
-          rules: [
-            {
-              test: /\.js$/,
-              use: {
-                loader: "babel-loader",
-                options: {
-                  presets: [
-                    ["@babel/preset-env", { targets: "> 0.25%, not dead" }],
-                  ],
+      gulpif(
+        PROD,
+        webpack({
+          module: {
+            rules: [
+              {
+                test: /\.js$/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: [
+                      ["@babel/preset-env", { targets: "> 0.25%, not dead" }],
+                    ],
+                  },
                 },
               },
-            },
-          ],
-        },
-        output: {
-          filename: PROD ? "main.min.js" : "main.js",
-        },
-        devtool: !PROD ? "inline-source-map" : false,
-        mode: PROD ? "production" : "development",
-      })
+            ],
+          },
+          output: {
+            filename: "main.min.js",
+          },
+          devtool: false,
+          mode: "production",
+        })
+      )
     )
     .pipe(bs.stream())
     .pipe(gulp.dest(paths.scripts.dest));
@@ -92,8 +115,30 @@ export const scripts = () => {
 export const icons = () => {
   return gulp
     .src(paths.icons.src)
-    .pipe(gulp.dest(paths.icons.dest))
+    .pipe(
+      gulpif(
+        PROD,
+        gulp.dest(paths.icons.dest.prod),
+        gulp.dest(paths.icons.dest.dev)
+      )
+    )
     .pipe(bs.stream());
+};
+
+export const images = () => {
+  return gulp
+    .src(paths.images.src)
+    .pipe(
+      imagemin([
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+      ])
+    )
+    .pipe(gulp.dest(paths.images.dest));
+};
+
+export const copy = () => {
+  return gulp.src("index.html").pipe(gulp.dest("dist"));
 };
 
 export const serve = (done) => {
@@ -110,12 +155,12 @@ export const reload = (done) => {
 };
 
 export const watch = () => {
-  gulp.watch("src/assets/scss/**/*.scss", styles);
-  gulp.watch("src/assets/js/**/*.js", scripts);
+  gulp.watch("assets/scss/**/*.scss", styles);
+  gulp.watch("assets/js/**/*.js", scripts);
   gulp.watch("**/*.html", reload);
 };
 
 export const dev = gulp.series(clean, styles, scripts, icons, serve, watch);
-export const prod = gulp.series(clean, styles, scripts, icons);
+export const prod = gulp.series(clean, styles, scripts, icons, images, copy);
 
 exports.default = dev;
