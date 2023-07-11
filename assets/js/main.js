e1826f2d36f6b9f292ffd8dfa4a2d589173cc3b6
ronngb <ronny.gabi@gmail.com>
2023-07-12 01:09:00 +0800

    feat: added Download() function for pdf format download

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
-document.body.onload = addPrintBtn();
+document.body.onload = cmdUtilities();
+
+function cmdUtilities() {
+  addPrintBtn()
+  addDownloadBtn() 
+}
 
 function addPrintBtn() {
   const elPrintBtn = document.createElement("button");
@@ -6,7 +11,7 @@ function addPrintBtn() {
 
   elPrintBtn.classList.add("print__button");
 
-  elPrintBtn.setAttribute("title", "Print");
+  elPrintBtn.setAttribute("title", "Print Resume");
 
   document.body.insertBefore(elPrintBtn, mainTag[0]);
 
@@ -14,3 +19,45 @@ function addPrintBtn() {
     print();
   });
 }
+
+function addDownloadBtn() {
+  const elDownloadBtn = document.createElement("a");
+  
+  const mainTag = document.getElementsByTagName("main");
+
+  elDownloadBtn.classList.add("download__button");
+
+  elDownloadBtn.setAttribute("title", "Download PDF");
+  
+  elDownloadBtn.setAttribute("href", "assets/pdf/ronn-front-end-web-developer.pdf");
+
+  elDownloadBtn.setAttribute("target", "_blank");
+
+  document.body.insertBefore(elDownloadBtn, mainTag[0]);
+
+  // elDownloadBtn.addEventListener("click", () => {
+  //   print();
+  // });
+}
+
+
+/* function addDownloadBtn() {
+  const elDownloadBtn = document.createElement("button");
+  // const elDownloadLink = document.createElement("a");
+  const mainTag = document.getElementsByTagName("main");
+
+  elDownloadBtn.classList.add("download__button");
+
+  // elDownloadBtn.appendChild(elDownloadLink);
+
+  elDownloadBtn.setAttribute("title", "Download PDF");
+  
+  elDownloadLink.setAttribute("href", "assets/pdf/test.pdf");
+  
+
+  document.body.insertBefore(elDownloadBtn, mainTag[0]);
+
+  // elDownloadBtn.addEventListener("click", () => {
+  //   print();
+  // });
+} */
