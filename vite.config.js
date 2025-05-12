8b5fbd360e0b8fbd1cec57ff95c125a65453f5b4
ronngb <ronny.gabi@gmail.com>
2025-05-12 16:35:53 +0800

    chore: init the scss build in vite config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  css: {
+    devSourcemap: true,
+    preprocessorOptions: {     
+      scss: {
+        api: "modern-compiler"
+      },
+    },
+  },
+});
